e767c126b4be0799ea97f1d77b063172a7d373b1
Malyarevskyi <pochta13a@gmail.com>
2016-07-07 19:28:50 +0300

    dz4_3
    home work

---
+++ DistanceBetweenPoints.java
@@ -0,0 +1,14 @@
+package dz4_3;
+
+public class DistanceBetweenPoints {
+
+
+    public static void main(String[] args) {
+        int x1=4;
+        int y1=5;
+        int x2=19;
+        int y2=35;
+        double a=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println (a);
+    }
+}
