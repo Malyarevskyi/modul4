a2c4f850ea155fdab7860cb45c0add194c8ef78d
Malyarevskyi <pochta13a@gmail.com>
2016-07-12 12:23:26 +0300

    dz4_3
    +ввод с консоли

--- DistanceBetweenPoints.java
+++ DistanceBetweenPoints.java
@@ -1,14 +1,25 @@
 package dz4_3;
 
+import java.util.Scanner;
+
 public class DistanceBetweenPoints {
 
 
     public static void main(String[] args) {
-        int x1=4;
-        int y1=5;
-        int x2=19;
-        int y2=35;
+        System.out.println("Для вычисления вычисления расстояния между точками введети их координаты");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите координаты первой точки");
+        System.out.println("Введите значение по шкале х");
+        double x1=scanner.nextDouble();
+        System.out.println("Введите значение по шкале у");
+        double y1=scanner.nextDouble();
+        System.out.println("Введите координаты второй точки");
+        System.out.println("Введите значение по шкале х");
+        double x2=scanner.nextDouble();
+        System.out.println("Введите значение по шкале у");
+        double y2=scanner.nextDouble();
+
         double a=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-        System.out.println (a);
+        System.out.println ("Расстояние между точками равно: "+a);
     }
 }
