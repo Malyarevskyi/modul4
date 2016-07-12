64e0b0df9dc958790d4e5124dbfd03d65501a6d7
Malyarevskyi <pochta13a@gmail.com>
2016-07-12 12:22:32 +0300

    dz4-2
    подправлено

---
+++ Calculation.java
@@ -0,0 +1,10 @@
+package dz4_2;
+
+public class Calculation {
+    public static double CelsiusToFahrenheit (double c) {
+        return (1.8*c+32);
+    }
+    public static double FahrenheitToCelsius (double f){
+        return ((f-32)/1.8);
+    }
+}
--- TransferTemperature.java
+++ TransferTemperature.java
@@ -1,18 +1,28 @@
 package dz4_2;
 
+import java.util.Scanner;
+
 public class TransferTemperature {
 
     public static void main(String[] args) {
-        System.out.println(CelsiusToFahrenheit());
-        System.out.println(FahrenheitToCelsius());
-    }
+        System.out.println("Для преобразования значение температуры по шкале Цельсия в значение по шкале Фаренгейта введите 1 \n"+
+                "Для преобразования значение температуры по шкале Фаренгейта в значение по шкале Цельсия введите 2 \n"+
+                "Для выхода 0\n");
+        Scanner scanner = new Scanner(System.in);
+        int scan=scanner.nextInt();
+            if (scan==1){
+                System.out.println("Введите  температура по шкале Цельсия: ");
+                double a = scanner.nextDouble();
+                System.out.println("Температура по шкале Фаренгейта равна " +Calculation.CelsiusToFahrenheit(a));
+            }
+            if (scan==1){
+                System.out.println("Введите  температура по шкале Фаренгейта: ");
+                double a = scanner.nextDouble();
+                System.out.println("Температура по шкале Цельсия равна " +Calculation.FahrenheitToCelsius(a));
+            }
+            if (scan==0){
+                System.exit(0);
+            }
 
-    public static double CelsiusToFahrenheit () {
-        double c=25;
-        return (1.8*c+32);
-    }
-    public static double FahrenheitToCelsius (){
-        double f=77;
-        return ((f-32)/1.8);
     }
 }
